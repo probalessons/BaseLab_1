143d7565d952bc1371c8b4f2cf399e61be699ec1
skopinvi <skopinvi@313D-15>
2022-09-03 12:17:38 +0700

    Завершение первого урока

--- BaseLab_1/Program.cs
+++ BaseLab_1/Program.cs
@@ -1,3 +1,23 @@
 ﻿using System;
 
-Console.WriteLine("Hello World!");
+int x, y;
+Console.WriteLine("Введите x");
+x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите y");
+y = Convert.ToInt32(Console.ReadLine());
+
+//Вариант 1
+//int z = x;
+//x = y;
+//y = z;
+
+//Вариант 2
+x = x - y;
+y = x + y;
+x = y - x;
+
+
+
+Console.WriteLine($"x = {x}, y = {y}");
+
+Console.ReadKey();
